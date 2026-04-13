52eafe86e4b2ea4fee851bd73e18414b931cdd6e
CATboardBETA <66703887+CATboardBETA@users.noreply.github.com>
2026-04-13 16:30:20 -0400

    revert x86_64 interrupt stuff so it at least compiles on x64

--- kernel/src/interrupt/interrupt_x64.rs
+++ kernel/src/interrupt/interrupt_x64.rs
@@ -1,16 +1,8 @@
-use crate::interrupt::interrupt_x64::acpi::AcpiHandlerImpl;
-use crate::interrupt::interrupt_x64::apic::init_io_apic;
 use crate::println;
-use crate::reqs::{HHDM, RSDP};
-use acpi::{AcpiTables, InterruptModel};
 use core::ops::IndexMut;
 use spin::Lazy;
-use x86_64::structures::idt::{
-    InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode,
-};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
-mod acpi;
-mod apic;
 mod irq;
 
 static IDT: Lazy<InterruptDescriptorTable> = Lazy::new(|| {
@@ -24,20 +16,6 @@ static IDT: Lazy<InterruptDescriptorTable> = Lazy::new(|| {
 });
 
 pub fn init_interrupt_table() {
-    disable_pic();
-    let phys_mem_offset = HHDM.response().unwrap().offset as usize;
-    let rsdp = RSDP.response().unwrap().address as usize - phys_mem_offset;
-    let handler = AcpiHandlerImpl::new();
-    let tables = unsafe { AcpiTables::from_rsdp(handler, rsdp).unwrap() };
-    let platform = tables.platform_info().unwrap();
-    let page_table = 
-    if let InterruptModel::Apic(apic) = platform.interrupt_model {
-        let io_apic_addr = apic.io_apics[0].address;
-        unsafe {
-            init_io_apic(io_apic_addr as _);
-        }
-    }
-
     IDT.load();
 }
 
@@ -66,13 +44,4 @@ extern "x86-interrupt" fn handle_alignment_check(
 }
 extern "x86-interrupt" fn handle_gpf(stack_frame: InterruptStackFrame, _error_code: u64) {
     println!("EXCEPTION: GENERAL PROTECTION FAULT\n{:#?}", stack_frame);
-}
-
-fn disable_pic() {
-    // Disable any unneeded PIC features, such as timer or keyboard to prevent it from firing interrupts
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        Port::<u8>::new(0xA1).write(0xFF); // PIC2 (Slave PIC)
-    }
 }
